77529a9408829c9964c595ad0ecac7c4563fadae
tarinya <muriele@trentini.ch>
2016-10-20 11:18:27 +0200

    adds member() for better readability

--- 1_try/base.pl
+++ 1_try/base.pl
@@ -1,13 +1,12 @@
-timeslot('08:00').
-timeslot('09:00').
+member(X,[X|T]).
+member(X,[_|T]):- member(X,T).
+
+timeslot(X):- member(X,['8:00','9:00']).
+modul(X):- member(X,['PrFm','An2I']).
+durchfuehrung(X):- member(X,['PrFmV1','PrFmV2','An2IV1','An2IV2']).
+
 timeorder('08:00', 1).
 timeorder('09:00', 2).
-modul('PrFm').
-modul('An2I').
-durchfuehrung('PrFmV1').
-durchfuehrung('PrFmV2').
-durchfuehrung('An2IV1').
-durchfuehrung('An2IV2').
 modulDurchfuehrung('PrFm', 'PrFmV1', '08:00').
 modulDurchfuehrung('PrFm', 'PrFmV2', '09:00').
 modulDurchfuehrung('An2I', 'An2IV1', '08:00').
