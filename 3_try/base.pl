44041282dd7d5de29885286d5465edfa476c3122
Raphi <raphiz@users.noreply.github.com>
2016-10-21 15:47:10 +0200

    Introduce order of timeslots

--- 3_try/base.pl
+++ 3_try/base.pl
@@ -1,14 +1,15 @@
-zeitslot('Mo', '08:00').
-zeitslot('Mo', '09:00').
-zeitslot('Di', '08:00').
-zeitslot('Di', '09:00').
+zeitslot(Tag, Zeit):- zeitslot(_, Tag, Zeit).
+zeitslot(1, 'Mo', '08:00').
+zeitslot(2, 'Mo', '09:00').
+zeitslot(3, 'Di', '08:00').
+zeitslot(4, 'Di', '09:00').
+
 
 lektion('PrFm', zeitslot('Mo', '08:00')).
 lektion('PrFm', zeitslot('Mo', '09:00')).
 lektion('An2I', zeitslot('Mo', '08:00')).
 lektion('An2I', zeitslot('Di', '08:00')).
 lektion('CldInf', zeitslot('Mo', '08:00')).
-%lektion('CldInf', zeitslot('Mo', '09:00')).
 lektion('CldInf', zeitslot('Di', '08:00')).
 
 stundenplan(Stundenplan):-
@@ -19,9 +20,9 @@ stundenplan(Stundenplan):-
 
 stundenplan(Module, Stundenplan):-
 	stundenplan(
-	  [ zeitslot('Mo', '08:00'),
-	    zeitslot('Mo', '09:00'),
-	    zeitslot('Di', '08:00')],
+	  [ zeitslot(1, 'Mo', '08:00'),
+	    zeitslot(2, 'Mo', '09:00'),
+	    zeitslot(3, 'Di', '08:00')],
 	  Module, Stundenplan
 	).
 
@@ -29,11 +30,11 @@ stundenplan(_, [], []).
 stundenplan(
     ZeitList,
     [Modul|ModulList],
-    [lektion(Modul,Zeit)|Stundenplan]
+    [lektion(Modul,zeitslot(NR, Tag, Zeit))|Stundenplan]
   ) :-
-	member(Zeit, ZeitList),
-	lektion(Modul, Zeit),
-	delete(ZeitList, Zeit, NewZeitList),
+	member(zeitslot(NR, Tag, Zeit), ZeitList),
+	lektion(Modul, zeitslot(Tag, Zeit)),
+	delete(ZeitList, zeitslot(NR, Tag, Zeit), NewZeitList),
 	stundenplan(NewZeitList, ModulList, Stundenplan).
 
 sortedStundenplan(Sorted):-
