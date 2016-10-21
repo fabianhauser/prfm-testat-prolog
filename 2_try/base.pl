f95b4597c2827e2207ae0990bd15c9cb7afdb74c
Fabian Hauser <fabian@fh2.ch>
2016-10-21 13:37:25 +0200

    Fix error and name cleanup

--- 2_try/base.pl
+++ 2_try/base.pl
@@ -1,27 +1,21 @@
-timeslot('Mo08:00').
-timeslot('Mo09:00').
-timeorder('Mo08:00', 1).
-timeorder('Mo09:00', 2).
-modul('PrFm').
-modul('An2I').
-durchfuehrung('PrFmV1').
-durchfuehrung('PrFmV2').
-durchfuehrung('An2IV1').
-durchfuehrung('An2IV2').
 modulDurchfuehrung('PrFm', 'PrFmV1', 'Mo08:00').
 modulDurchfuehrung('PrFm', 'PrFmV2', 'Mo09:00').
 modulDurchfuehrung('An2I', 'An2IV1', 'Mo08:00').
-modulDurchfuehrung('An2I', 'An2IV2', 'Mo09:00').
+modulDurchfuehrung('An2I', 'An2IV2', 'Mo10:00').
+modulDurchfuehrung('CldInf', 'CldInfV1', 'Mo08:00').
+%modulDurchfuehrung('CldInf', 'CldInfV2', 'Mo09:00').
+modulDurchfuehrung('CldInf', 'CldInfV2', 'Mo10:00').
 
-stundenplan([], [], _).
+stundenplan(Module, Stundenplan):- stundenplan(['Mo08:00','Mo09:00','Mo10:00'], Module, Stundenplan).
+
+stundenplan(_, [], _).
 stundenplan(
     ZeitList,
-    [Modul|StrikeList],
-    [lektion(Modul,Zeit)|LektionList]
+    [Modul|ModulList],
+    [lektion(Modul,Zeit)|Stundenplan]
   ) :-
-	member(Zeit, _ZeitList),
+	member(Zeit, ZeitList),
 	modulDurchfuehrung(Modul, _, Zeit),
-	modul(Modul),
-	timeslot(Zeit),
 	delete(ZeitList, Zeit, NewZeitList),
-	stundenplan(NewZeitList, StrikeList, LektionList).
+	stundenplan(NewZeitList, ModulList, Stundenplan).
+
