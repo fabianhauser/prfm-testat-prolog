40a5119293c2efb5c52bdd7f2860ce87745f76f2
tarinya <muriele@trentini.ch>
2016-10-23 12:38:57 +0200

    final hand-in version: adds comments and example-queries

--- 3_try/base.pl
+++ 3_try/base.pl
@@ -1,10 +1,20 @@
+% Interest Base Project In Prolog
+% Participants: Raphael Zimmermann, Fabian Hauser, Muriele Trentini
+% Project: We would like to explore how Prolog can be used to create a class schedule for students.
+
+% use with swish [ http://swish.swi-prolog.org/ ] to render a nice table
+:- use_rendering(table,
+		 [header(lektion('Modul', 'Zeitslot'))]).
+
+
+% All empty TimeSlots i.e. the week
 zeitslot(Tag, Zeit):- zeitslot(_, Tag, Zeit).
 zeitslot(1, 'Mo', '08:00').
 zeitslot(2, 'Mo', '09:00').
 zeitslot(3, 'Di', '08:00').
 zeitslot(4, 'Di', '09:00').
 
-
+% All available courses
 lektion('PrFm', zeitslot('Mo', '08:00')).
 lektion('PrFm', zeitslot('Mo', '09:00')).
 lektion('An2I', zeitslot('Mo', '08:00')).
@@ -20,7 +30,7 @@ allZeitslot(Zeitslots):-
 	findall(zeitslot(X,Y,Z), zeitslot(X,Y,Z), ZeitslotsBag),
 	sort(ZeitslotsBag, Zeitslots).
 
-
+% stundenplan with different arities for convenient queries
 stundenplan(Stundenplan):-
 	allLektionen(Lektionen), 
 	stundenplan(
@@ -49,3 +59,8 @@ sortedStundenplan(Sorted):-
 	stundenplan(S),
 	sort(2, @=<, S, Sorted).
 
+/* query-examples:
+	sortedStundenplan(Sorted).
+	stundenplan(['PrFm', 'An2I'], Stundenplan).	
+	allLektionen(Lektionen).
+*/
